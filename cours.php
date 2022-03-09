eb0cc281453e9e810e1e3dd950ed0bbb4c78c175
laurion <klauriondareich@gmail.com>
2022-03-09 19:33:40 +0100

    elements reorganized

--- cours.php
+++ cours.php
@@ -27,7 +27,7 @@
 
         <div class="main-content">
             <div class="course-header">
-                <h1>Programmation avec Python</h1>
+                <h1>Programmation avec Python/C</h1>
                 <p>Python est un langage de programmation inventé par Guido van Rossum...</p>
             </div>
             
@@ -35,30 +35,22 @@
 
                 <div class="course-item">
                     <div class="first-part" style="padding-bottom: 30px;">
-                        <h3>Exerice 1</h3>
+                        <h3>Exerice 1 (avec python)</h3>
                         <p>Les fondamentaux de la programmation avec Python</p>
                         <a class="btn-style" href="assets/docs/lpic-python-exercice.pdf">Voir l'exercice</a>
-                        <a class="btn-style" href="">Soumettre le devoir</a>
+                        <a class="btn-style" href="form.php">Soumettre le devoir</a>
 
                     </div>
                 </div>
                 <div class="course-item">
                     <div class="first-part" style="padding-bottom: 30px;">
-                        <h3>Exerice 2</h3>
-                        <p>La programmation avancée avec python</p>
+                        <h3>Exerice 2 (avec le C)</h3>
+                        <p>Les fondamentaux de la programmation avec C</p>
                         <a class="btn-style" href="assets/docs/lpic-python-exercice.pdf">Voir l'exercice</a>
-                        <a class="btn-style" href="">Soumettre le devoir</a>
+                        <a class="btn-style" href="form.php">Soumettre le devoir</a>
                     </div>
                 </div>
 
-                <div class="course-item">
-                    <div class="first-part" style="padding-bottom: 30px;">
-                        <h3>Exerice 3</h3>
-                        <p>Paradigm diviser pour régner en python</p>
-                        <a class="btn-style" href="#">Voir l'exercice</a>
-                        <a class="btn-style" href="#">Soumettre le devoir</a>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
