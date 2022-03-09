a7c1dfb5853a7702b6e7ee2ab5926013262f160d
laurion <klauriondareich@gmail.com>
2022-03-09 12:28:54 +0100

    login page added

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main-container-login">
+        <form action="post">
+            <input type="text" placeholder="Saisir  l'adresse email">
+            <input type="password" placeholder="Saisir  le mot de passe">
+            <button>Se connecter</button>
+        </form>
+    </div>
+</body>
+</html>
