2bb8a3c1b7420025e4d5fc16b0d144a5a5be9d89
laurion <klauriondareich@gmail.com>
2022-03-09 12:21:30 +0100

    Initial project

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LPIC</title>
+</head>
+<body>
+    <div>
+        <nav>
+            <ul>
+                <li>Accueil</li>
+                <li>Mes cours</li>
+                <li>Username</li>
+                <li>Se déconnecter</li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="main-container">
+        <h1>Bienvenue sur Supinfo Learning</h1>
+        <p>Apprenez à votre rythme où que vous soyez</p>
+        <button>Allez au cours</button>
+    </div>
+</body>
+</html>
