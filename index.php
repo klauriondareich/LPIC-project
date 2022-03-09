0d0a5b620bc2b250a3fd475fe7ffab96cf00859f
laurion <klauriondareich@gmail.com>
2022-03-09 13:25:54 +0100

    new html structure added to design course block

--- index.php
+++ index.php
@@ -5,23 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LPIC</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <div>
+    <div class="main-home-container">
         <nav>
             <ul>
-                <li>Accueil</li>
                 <li>Mes cours</li>
                 <li>Username</li>
-                <li>Se déconnecter</li>
+                <li class="logout">
+                    <a href="">
+                    Se déconnecter
+                    </a>
+                </li>
             </ul>
         </nav>
     </div>
 
     <div class="main-container">
-        <h1>Bienvenue sur Supinfo Learning</h1>
-        <p>Apprenez à votre rythme où que vous soyez</p>
-        <button>Allez au cours</button>
+        <div class="course-header">
+            <h1>Mes cours</h1>
+            <p>Apprenez à votre rythme où que vous soyez.</p>
+        </div>
+        
+        <div class="courses-block">
+            <div class="course-item">
+                <h3>Programmation orienté objet</h3>
+                <p>Description sur la programmation orienté objet</p>
+                <button>Allez au cours</button>
+            </div>
+        </div>
     </div>
 </body>
 </html>
