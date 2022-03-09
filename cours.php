056166cf1a6ef6f066a37b17ee2aeada1615adc9
laurion <klauriondareich@gmail.com>
2022-03-09 16:55:43 +0100

    cours page added

---
+++ cours.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LPIC</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body style="background: #f5f7f8;">
+    <div class="main-home-container">
+       
+        <nav>
+            <ul>
+                <li>Mes cours</li>
+                <li>Username</li>
+                <li class="logout">
+                    <a href="">
+                    Se déconnecter
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+        <div class="bg-image">
+        </div>
+
+        <div class="main-content">
+            <div class="course-header">
+                <h1>Programmation orienté objet</h1>
+                <p>Description sur la programmation orienté objet Description sur la programmation orienté objet</p>
+            </div>
+            
+            <div>
+                
+            </div>
+            <div class="courses-block">
+
+                <div class="course-item">
+                    <div class="first-part" style="padding-bottom: 30px;">
+                        <h3>Exerice 1</h3>
+                        <p>Les fondamentaux de la programmation avec Python</p>
+                        <a class="btn-style" href="assets/docs/lpic-python-exercice.pdf">Voir l'exercice</a>
+                        <a class="btn-style" href="">Soumettre le devoir</a>
+
+                    </div>
+                </div>
+                <div class="course-item">
+
+                    <div class="first-part" style="padding-bottom: 30px;">
+                        <h3>Exerice 2</h3>
+                        <p>La programmation avancée avec python</p>
+                        <a class="btn-style" href="assets/docs/lpic-python-exercice.pdf">Voir l'exercice</a>
+                        <a class="btn-style" href="">Soumettre le devoir</a>
+                    </div>
+                </div>
+
+                <div class="course-item">
+
+                    <div class="first-part" style="padding-bottom: 30px;">
+                        <h3>Exerice 3</h3>
+                        <p>Paradigm diviser pour régner en python</p>
+                        <a class="btn-style" href="assets/docs/lpic-python-exercice.pdf">Voir l'exercice</a>
+                        <a class="btn-style" href="assets/docs/lpic-python-exercice.pdf">Soumettre le devoir</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
