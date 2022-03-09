54c4309377c7b1654352c85d534af9c6d846906c
laurion <klauriondareich@gmail.com>
2022-03-09 12:55:02 +0100

    title added to login form, some classes added to style tags

--- login.php
+++ login.php
@@ -5,10 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css"> 
 </head>
-<body>
+<body class="login-body">
     <div class="main-container-login">
-        <form action="post">
+        <form action="post" id="login-form">
+            <h3>Connexion</h3>
             <input type="text" placeholder="Saisir  l'adresse email">
             <input type="password" placeholder="Saisir  le mot de passe">
             <button>Se connecter</button>
